c7f0e08cec2b2662af54ad618d297d1c8365cffe
GuilhermeBarbosa16 <guiba1616@gmail.com>
2024-07-29 22:08:38 -0300

    Feat: Criação do layout

--- src/Pages/_layouts/app.tsx
+++ src/Pages/_layouts/app.tsx
@@ -1,20 +1,169 @@
-import { AppBar, Toolbar, IconButton, Typography, Button } from "@mui/material";
-import { Outlet } from "react-router-dom";
-import Header from "../../components/header";
+import { useState } from 'react';
+import {
+  Box,
+  Drawer as MuiDrawer,
+  IconButton,
+  List,
+  Toolbar,
+  Typography,
+  Divider,
+  Button,
+  AppBar as MuiAppBar,
+  AppBarProps,
+  ListItem,
+  ListItemIcon,
+  ListItemText
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PeopleIcon from '@mui/icons-material/People';
+import { styled } from '@mui/material/styles';
+import { Outlet } from 'react-router-dom';
+
+const drawerWidth = 240;
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    '& .MuiDrawer-paper': {
+      position: 'relative',
+      whiteSpace: 'nowrap',
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      boxSizing: 'border-box',
+      ...(!open && {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up('sm')]: {
+          width: theme.spacing(9),
+        },
+      }),
+    },
+  })
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
 
 export function AppLayout() {
-    return (
-        <>
-           <div>
-           <Header /> 
-           </div>
-            <div>
-                <h1>Dashboard</h1>
-            </div>
+  const [open, setOpen] = useState(true);
 
-            <div>
-                <Outlet />
-            </div>
-        </>
-    );
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <AppBar position="absolute" open={open} sx={{ backgroundColor: '#FFF', boxShadow: 'none' }}>
+        <Toolbar sx={{ pr: '24px' }}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer}
+            sx={{
+              marginRight: '36px',
+              ...(open && { display: 'none' }),
+            }}
+          >
+            <MenuIcon sx={{ color: 'black' }} />
+          </IconButton>
+          <Typography component="h1" variant="h6" color="#000000" noWrap sx={{ flexGrow: 1 }}>
+            Dashboard
+          </Typography>
+          <IconButton>
+            <AccountCircleIcon />
+            <span style={{ fontSize: '0.8rem' }}>test@gmail.com</span>
+          </IconButton>
+          <Button
+            href='/sign-in'
+            style={{
+              background: '#00C2FF',
+              fontFamily: 'Open Sans, sans-serif',
+              fontSize: '16px', color: '#000000',
+              marginTop: '-2px', fontWeight: 'bold'
+            }}
+          >
+            Sair
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <Toolbar
+          sx={{ 
+            display: 'flex', 
+            alignItems: 'center', 
+            justifyContent: 'space-between', 
+            px: [1] 
+          }}
+        >
+          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+            <Typography variant="h5" color="#00C2FF" sx={{ fontWeight: 'bold' }}>
+              FinWise
+            </Typography>
+            <Typography variant="subtitle2" color="#00C2FF" sx={{ marginTop: -1 }}>
+              financial
+            </Typography>
+          </Box>
+          <IconButton onClick={toggleDrawer}>
+            <ChevronLeftIcon sx={{ color: 'black' }} />
+          </IconButton>
+        </Toolbar>
+        <Divider />
+        <List component="nav">
+          <ListItem button>
+            <ListItemIcon>
+              <DashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <PeopleIcon />
+            </ListItemIcon>
+            <ListItemText primary="Customers" />
+          </ListItem>
+          <Divider sx={{ my: 1 }} />
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'light'
+              ? theme.palette.grey[100]
+              : theme.palette.grey[900],
+          flexGrow: 1,
+          height: '100vh',
+          overflow: 'auto',
+        }}
+      >
+        <Toolbar />
+        <Outlet />
+      </Box>
+    </Box>
+  );
 }
--- src/Pages/app/Dashboard/DashboardHome.tsx
+++ src/Pages/app/Dashboard/DashboardHome.tsx
@@ -1,20 +1,11 @@
 import * as React from 'react';
-import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import MuiDrawer from '@mui/material/Drawer';
-import Box from '@mui/material/Box';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
-import Link from '@mui/material/Link';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import AddIcon from '@mui/icons-material/Add';
@@ -30,11 +21,7 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-import { mainListItems, secondaryListItems } from './listItems';
-import Deposits from './Deposits';
-import Total from './Total';
-import Saidas from './Saidas';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { IconButton, Link } from '@mui/material';
 
 function Copyright(props: any) {
   return (
@@ -49,82 +36,24 @@ function Copyright(props: any) {
   );
 }
 
-const drawerWidth: number = 240;
-
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    '& .MuiDrawer-paper': {
-      position: 'relative',
-      whiteSpace: 'nowrap',
-      width: drawerWidth,
-      transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      boxSizing: 'border-box',
-      ...(!open && {
-        overflowX: 'hidden',
-        transition: theme.transitions.create('width', {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen,
-        }),
-        width: theme.spacing(7),
-        [theme.breakpoints.up('sm')]: {
-          width: theme.spacing(9),
-        },
-      }),
-    },
-  }),
-);
-
 const defaultTheme = createTheme();
 
 export default function DashboardHome() {
-  const [open, setOpen] = React.useState(true);
   const [searchQuery, setSearchQuery] = React.useState('');
   const [transactions, setTransactions] = React.useState([
     { type: 'Entrada', summary: 'Desenvolvimento de site', date: '28/04/2024', value: 'R$ 10,000.00' },
     { type: 'Saida', summary: 'Hamburger', date: '29/04/2024', value: 'R$ 80.00' },
   ]);
 
-  const toggleDrawer = () => {
-    setOpen(!open);
-  };
-
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(event.target.value);
   };
 
   const handleEdit = (index: number) => {
-    // Lógica para editar a transação
     console.log('Editar transação:', index);
   };
 
   const handleDelete = (index: number) => {
-    // Lógica para deletar a transação
-    console.log('Deletar transação:', index);
     setTransactions(transactions.filter((_, i) => i !== index));
   };
 
@@ -134,192 +63,130 @@ export default function DashboardHome() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <AppBar position="absolute" open={open} sx={{ backgroundColor: '#FFF', boxShadow: 'none' }}>
-          <Toolbar sx={{ pr: '24px' }}>
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
-                marginRight: '36px',
-                ...(open && { display: 'none' }),
-              }}
-            >
-              <MenuIcon sx={{ color: 'black' }} />
-            </IconButton>
-            <Typography component="h1" variant="h6" color="#000000" noWrap sx={{ flexGrow: 1 }}>
-              Dashboard
-            </Typography>
-            <IconButton>
-              <AccountCircleIcon />
-              <span style={{ fontSize: '0.8rem' }}>test@gmail.com</span>
-            </IconButton>
-            <Button
-              href='/sign-in'
-              style={{
-                background: '#00C2FF',
-                fontFamily: 'Open Sans, sans-serif',
-                fontSize: '16px', color: '#000000',
-                marginTop: '-2px', fontWeight: 'bold'
-              }}
-            >
-              Sair
-            </Button>
-          </Toolbar>
-        </AppBar>
-        <Drawer variant="permanent" open={open}>
-          <Toolbar sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', px: [1] }}>
-            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-              <Typography variant="h5" color="#00C2FF" sx={{ fontWeight: 'bold' }}>
-                FinWise
-              </Typography>
-              <Typography variant="subtitle2" color="#00C2FF" sx={{ marginTop: -1 }}>
-                financial
-              </Typography>
+      <CssBaseline />
+      <Box sx={{ display: 'flex', flexGrow: 1 }}>
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 2 }}>
+            <Box sx={{ border: '1px solid #000000', padding: '8px', borderRadius: '4px' }}>Janeiro</Box>
+            <Box>
+              <Button variant="contained" color="primary" sx={{ mr: 1 }} startIcon={<AddIcon />}>
+                Nova Transação
+              </Button>
+              <Button variant="contained" color="success" startIcon={<DownloadIcon />}>
+                Baixar Excel
+              </Button>
             </Box>
-            <IconButton onClick={toggleDrawer}>
-              <ChevronLeftIcon sx={{ color: 'black' }} />
-            </IconButton>
-          </Toolbar>
-          <Divider />
-          <List component="nav">
-            {mainListItems}
-            <Divider sx={{ my: 1 }} />
-            {secondaryListItems}
-          </List>
-        </Drawer>
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === 'light'
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            height: '100vh',
-            overflow: 'auto',
-          }}
-        >
-          <Toolbar />
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 2 }}>
-              <Box sx={{ border: '1px solid #000000', padding: '8px', borderRadius: '4px' }}>Janeiro
-              </Box>
-              <Box>
-                <Button variant="contained" color="primary" sx={{ mr: 1 }} startIcon={<AddIcon />}>
-                  Nova Transação
-                </Button>
-                <Button variant="contained" color="success" startIcon={<DownloadIcon />}>
-                  Baixar Excel
-                </Button>
-              </Box>
-            </Box>
-            <Grid container spacing={3} justifyContent="center">
-              <Grid item xs={12} md={4} lg={3}>
-                <Paper
-                  sx={{
-                    color: "white",
-                    backgroundColor: '#454545',
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 150,
-                  }}
-                >
-                  <Deposits />
-                </Paper>
-              </Grid>
-              <Grid item xs={12} md={4} lg={3}>
-                <Paper
-                  sx={{
-                    color: "white",
-                    backgroundColor: '#454545',
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 150,
-                  }}
-                >
-                  <Saidas />
-                </Paper>
-              </Grid>
-              <Grid item xs={12} md={4} lg={3}>
-                <Paper
-                  sx={{
-                    color: "white",
-                    backgroundColor: '#454545',
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 150,
-                  }}
-                >
-                  <Total />
-                </Paper>
-              </Grid>
-              <Grid item xs={12}>
-                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                  <TextField
-                    label="Busque por Transações"
-                    variant="outlined"
-                    fullWidth
-                    value={searchQuery}
-                    onChange={handleSearchChange}
-                    InputProps={{
-                      endAdornment: (
-                        <InputAdornment position="end">
-                          <IconButton>
-                            <SearchIcon />
-                          </IconButton>
-                        </InputAdornment>
-                      ),
-                    }}
-                  />
-                  <Table sx={{ mt: 2 }}>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>Tipo</TableCell>
-                        <TableCell>Resumo</TableCell>
-                        <TableCell>Data</TableCell>
-                        <TableCell>Valor</TableCell>
-                        <TableCell>Ações</TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {filteredTransactions.map((transaction, index) => (
-                        <TableRow key={index}>
-                          <TableCell>
-                            {transaction.type === 'Entrada' ? (
-                              <ArrowDownwardIcon sx={{ color: 'green' }} />
-                            ) : (
-                              <ArrowUpwardIcon sx={{ color: 'red' }} />
-                            )}
-                            {transaction.type}
-                          </TableCell>
-                          <TableCell>{transaction.summary}</TableCell>
-                          <TableCell>{transaction.date}</TableCell>
-                          <TableCell>{transaction.value}</TableCell>
-                          <TableCell>
-                            <IconButton onClick={() => handleEdit(index)}>
-                              <EditIcon />
-                            </IconButton>
-                            <IconButton onClick={() => handleDelete(index)}>
-                              <DeleteIcon />
-                            </IconButton>
-                          </TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </Paper>
-              </Grid>
+          </Box>
+          <Grid container spacing={3} justifyContent="center">
+            <Grid item xs={12} md={4} lg={3}>
+              <Paper
+                sx={{
+                  color: '#FFF',
+                  p: 2,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  height: 120,
+                  backgroundColor: '#333333',
+                }}
+              >
+                <Typography variant="h6" gutterBottom>
+                  Entradas
+                </Typography>
+                <Typography variant="h4">$3,024.00</Typography>
+              </Paper>
             </Grid>
-            <Copyright sx={{ pt: 4 }} />
-          </Container>
-        </Box>
+            <Grid item xs={12} md={4} lg={3}>
+              <Paper
+                sx={{
+                  color: '#FFF',
+                  p: 2,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  height: 120,
+                  backgroundColor: '#333333',
+                }}
+              >
+                <Typography variant="h6" gutterBottom>
+                  Saidas
+                </Typography>
+                <Typography variant="h4">$3,024.00</Typography>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={4} lg={3}>
+              <Paper
+                sx={{
+                  color: '#FFF',
+                  p: 2,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  height: 120,
+                  backgroundColor: '#333333',
+                }}
+              >
+                <Typography variant="h6" gutterBottom>
+                  Total
+                </Typography>
+                <Typography variant="h4">$3,024.00</Typography>
+              </Paper>
+            </Grid>
+            <Grid item xs={12}>
+              <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                <TextField
+                  label="Busque por Transações"
+                  variant="outlined"
+                  fullWidth
+                  value={searchQuery}
+                  onChange={handleSearchChange}
+                  sx={{ mb: 2 }}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <SearchIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                <Table size="small">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Tipo</TableCell>
+                      <TableCell>Resumo</TableCell>
+                      <TableCell>Data</TableCell>
+                      <TableCell>Valor</TableCell>
+                      <TableCell>Ações</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {filteredTransactions.map((transaction, index) => (
+                      <TableRow key={index}>
+                        <TableCell>
+                          {transaction.type === 'Entrada' ? (
+                            <ArrowDownwardIcon style={{ color: 'green' }} />
+                          ) : (
+                            <ArrowUpwardIcon style={{ color: 'red' }} />
+                          )}{' '}
+                          {transaction.type}
+                        </TableCell>
+                        <TableCell>{transaction.summary}</TableCell>
+                        <TableCell>{transaction.date}</TableCell>
+                        <TableCell>{transaction.value}</TableCell>
+                        <TableCell>
+                          <IconButton onClick={() => handleEdit(index)}>
+                            <EditIcon />
+                          </IconButton>
+                          <IconButton onClick={() => handleDelete(index)}>
+                            <DeleteIcon />
+                          </IconButton>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </Paper>
+            </Grid>
+          </Grid>
+          <Copyright sx={{ pt: 4 }} />
+        </Container>
       </Box>
     </ThemeProvider>
   );
